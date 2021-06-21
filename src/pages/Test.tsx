91d238edb341756c772f196907a6a64408f6056c
wangchong <15032368272@163.com>
2021-06-21 14:34:49 +0800

    test

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,20 +1,8 @@
 import React from 'react';
-import { PageContainer } from '@ant-design/pro-layout';
-import { Card, Alert, Typography, message } from 'antd';
-import { useIntl, FormattedMessage } from 'umi';
-import styles from './Welcome.less';
 
-const CodePreview: React.FC = ({ children }) => (
-  <pre className={styles.pre}>
-    <code>
-      <Typography.Text copyable>{children}</Typography.Text>
-    </code>
-  </pre>
-);
+import { Card, message } from 'antd';
 
 export default (): React.ReactNode => {
-  const intl = useIntl();
-
   React.useEffect(() => {
     message.success('多次渲染');
   }, []);
